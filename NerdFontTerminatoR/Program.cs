6230a6d5ef712793d3967ddd22794604d72347ef
Benito Palacios <benito356@gmail.com>
2013-05-30 15:05:44 +0200

    Added licence header in files.

--- NerdFontTerminatoR/Program.cs
+++ NerdFontTerminatoR/Program.cs
@@ -1,3 +1,25 @@
+// -----------------------------------------------------------------------
+// <copyright file="Program.cs" company="none">
+// Copyright (C) 2013
+//
+//   This program is free software: you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by 
+//   the Free Software Foundation, either version 3 of the License, or
+//   (at your option) any later version.
+//
+//   This program is distributed in the hope that it will be useful, 
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details. 
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program.  If not, see "http://www.gnu.org/licenses/". 
+// </copyright>
+// <author>pleoNeX</author>
+// <email>benito356@gmail.com</email>
+// <date>30/05/2013</date>
+// -----------------------------------------------------------------------
+
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -29,7 +51,7 @@ namespace Nftr
 		{
 			string fin   = "/home/benito/Dropbox/Ninokuni español/Fuentes/";
 			string fout  = "/home/benito/Dropbox/Ninokuni español/Fuentes/";
-			string fname = "font_b9";
+			string fname = "font_b32";
 
 			NftrFont fold = new NftrFont(fin + fname + ".NFTR");
 			fold.Export(fout + fname + "_.xml", fout + fname + "_.png");
