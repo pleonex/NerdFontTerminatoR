4a63645208afcad79cd00002df42a7b0a79bad82
Benito Palacios Sánchez <benito356@gmail.com>
2016-01-03 17:57:09 +0100

    Bump version

--- NerdFontTerminatoR/Properties/AssemblyInfo.cs
+++ NerdFontTerminatoR/Properties/AssemblyInfo.cs
@@ -28,11 +28,11 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Cambie estos atributos para modificar la información
 // asociada con un ensamblado.
 [assembly: AssemblyTitle("NerdFontTerminatoR")]
-[assembly: AssemblyDescription("NFTR creator & editor")]
+[assembly: AssemblyDescription("NFTR creator and editor")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © pleonex 2013")]
+[assembly: AssemblyCopyright("Copyright © pleonex 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -54,5 +54,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de versión de compilación y de revisión 
 // mediante el asterisco ('*'), como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
